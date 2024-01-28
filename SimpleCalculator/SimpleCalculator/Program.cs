f4fb489cf7cf893319c589fd9f551371e367a390
Illia Kotviskyi <iiiako197@gmail.com>
2024-01-28 12:55:47 +0200

    gitignore + calculator

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: CalculatorApp <int operand1> <operator> <int operand2>");
+                Environment.Exit(int.MinValue);
+            }
+
+            if (int.TryParse(args[0], out int value1) && int.TryParse(args[2], out int value2))
+            {
+                switch (args[1])
+                {
+                    case "+":
+                        Environment.Exit(value1 + value2);
+                        break;
+
+                    case "-":
+                        Environment.Exit(value1 - value2);
+                        break;
+
+                    case "*":
+                        Environment.Exit(value1 * value2);
+                        break;
+
+                    case "/":
+                        if (value2 == 0)
+                        {
+                            Console.WriteLine("Error: Division by zero");
+                            Environment.Exit(int.MinValue);
+                        }
+                        else
+                        {
+                            Environment.Exit(value1 / value2);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Error: invalid operator format");
+                        Environment.Exit(int.MinValue);
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid input format");
+                Environment.Exit(int.MinValue);
+            }
+        }
+    }
+}
